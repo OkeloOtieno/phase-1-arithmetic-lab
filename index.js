5be273ca5a864a440964a953db522dca104150aa
Emmanuel <emmanuelokello294@gmail.com>
2024-03-18 21:08:27 +0300

    completes arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 2;
+const num2 = 31;
+let num3 = 10;
+let num4 = 6;
+let num5 = 20;
 
-// Write your code here
+let multiply = num1 * num2;
+let random = Math.floor(Math.random() * 10); 
+let mod = num3 % num4;
+let max = Math.max(num1, num3, num4, num5)
+
+console.log(max)
